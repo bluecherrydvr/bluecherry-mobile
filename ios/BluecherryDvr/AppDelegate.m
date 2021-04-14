0e13f70baeeb7590d87551760cdbe74fea78442a
Tugrul Topuz <tugrultopuz@gmail.com>
2021-04-14 05:06:15 +0300

    ios push notification config

--- ios/BluecherryDvr/AppDelegate.m
+++ ios/BluecherryDvr/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
 #import "Orientation.h"
 
 #ifdef FB_SONARKIT_ENABLED
@@ -28,6 +29,10 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
